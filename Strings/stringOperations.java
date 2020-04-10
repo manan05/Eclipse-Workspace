e3331f25ccb09160ae463a1d36f91c1cd4bb61c7
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-04-11 04:58:56 +0530

    Create stringOperations.java

---
+++ Strings/stringOperations.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class stringOperations {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		printChars(str);
+	}
+
+	public static void printChars(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i));
+		}
+	}
+
+}

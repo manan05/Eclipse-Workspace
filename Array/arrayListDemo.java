8922a6a5bd4220b37a025035e7edefa0a1aee6a9
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-04-15 06:17:09 +0530

    Update arrayListDemo.java

--- Array/arrayListDemo.java
+++ Array/arrayListDemo.java
@@ -5,27 +5,46 @@ import java.util.ArrayList;
 public class arrayListDemo {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<>(); 
-		System.out.println(list);
-		// output = [] empty array
-		System.out.println(list.size());
-		// 0 because no element.
-		list.add(10);
-		System.out.println(list);
-		//[10]
-		System.out.println(list.size());
-		//1
-		list.add(20);
-		System.out.println(list);
-		//[10,20]
-		System.out.println(list.size());
-		// 2 
-		list.add(30);
-		System.out.println(list);
-		//[10,20,30]
-		System.out.println(list.size());
-		// 3
+//		ArrayList<Integer> list = new ArrayList<>();
+//		System.out.println(list);
+//		// output = [] empty array
+//		System.out.println(list.size());
+//		// 0 because no element.
+//		list.add(10);
+//		System.out.println(list);
+//		// [10]
+//		System.out.println(list.size());
+//		// 1
+//		list.add(20);
+//		System.out.println(list);
+//		// [10,20]
+//		System.out.println(list.size());
+//		// 2
+//		list.add(30);
+//		System.out.println(list);
+//		// [10,20,30]
+//		System.out.println(list.size());
+//		// 3
+		
+		int[] one = {10,10,20,40,40,50,50};
+	}
+
+	public static ArrayList<Integer> Intersection(int[] one, int[] two) {
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		for (int i = 0, j = 0; i < one.length && j < two.length;) {
+			if (one[i] == two[j]) {
+				ans.add(one[i]);
+				i++;
+				j++;
+			} else if (one[i] > two[j]) {
+				j++;
+			}
+			else {
+				i++;
+			}
+		}
+		return ans;
+
 	}
 
 }
-

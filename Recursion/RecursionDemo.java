59169fd57a95ab286450f4ff53e5c4b9a9b4545d
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-04-19 10:30:04 +0530

    printIncreasing()

--- Recursion/RecursionDemo.java
+++ Recursion/RecursionDemo.java
@@ -3,25 +3,36 @@ package Recursion;
 public class RecursionDemo {
 
 	public static void main(String[] args) {
-		printDec(5);
-
+//		printDec(5);
+		printInc(5);
 	}
+
 	public static void printDec(int n) {
-		//Base Case
-		//-gets StackOverflow Error that means
-		//-base case not handled properly
-		if(n == 0) {
+		// Base Case
+		// -gets StackOverflow Error that means
+		// -base case not handled properly
+		if (n == 0) {
 			return;
 		}
-		
-		//Self Work
+
+		// Self Work
 		System.out.println(n);
-		
-		//Smaller Input
-		printDec(n-1);
-		
-		
-		
+
+		// Smaller Input
+		printDec(n - 1);
+	}
+
+	public static void printInc(int n) {
+		// Base Case
+		if (n == 0) {
+			return;
+		}
+
+		// Smaller Input
+		printInc(n - 1);
+
+		// Self Work
+		System.out.println(n);
 	}
 
 }

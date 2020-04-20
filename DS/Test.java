c64e239a60194ec9fac82cf09091abc418ca48bc
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-04-20 10:14:41 +0530

    Update Test.java

--- DS/Test.java
+++ DS/Test.java
@@ -3,40 +3,26 @@ package DS;
 public class Test {
 
 	public static void main(String[] args) {
-//		System.out.println("TwoLoops");
-//		twoLoops();
-//		System.out.println("TwoVarsinSameLoop");
-//		twoVarsinSameLoop();
-		int[] arr = {2,-1,3,5,6,-2};
-		twoVarsinSameLoop(arr);
+		System.out.println("TwoLoops");
+		twoLoops();
+		System.out.println("TwoVarsinSameLoop");
+		twoVarsinSameLoop();
 
 	}
+	// in this we coded two loops and checked the 
+	// values of i and j printed
 	public static void twoLoops() {
-		for(int i = 0; i<5;i++) {
-			for(int j = i+1;j<5;j++) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = i + 1; j < 5; j++) {
 				System.out.println(i + "+" + j);
 			}
 		}
 	}
-	
-	public static void twoVarsinSameLoop(int[] arr) {
-		int max = Integer.MIN_VALUE;
-		int ni = 0;
-		int nj = 0;
-		for(int i = 0,j = i+1; i<arr.length && j<arr.length ;i++,j++) {
-			System.out.println(arr[i] + "+" + arr[j]);
-			int sum = arr[i] + arr[j];
-			if(sum > max) {
-				max= sum;
-				ni = i;
-				nj = j;
-			}
-//			System.out.println(arr[i]+arr[j]);
-		}
-		System.out.println(max);
-		System.out.println(arr[ni] + "+" + arr[nj]);
-		
-	}
-	
 
+	public static void twoVarsinSameLoop() {
+		for (int i = 0, j = i + 1; i < 5 && j < 5; i++, j++) {
+			System.out.println(i + "+" + j);
+		}
+
+	}
 }

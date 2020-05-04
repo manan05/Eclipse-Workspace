ded2ad0a69eb91ff4cc7bbbce4ea74f07e9277e7
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-05-05 04:20:28 +0530

    Update Stack.java

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,5 +1,20 @@
 package Stack;
 
 public class Stack {
-
+	int tos;
+	int[] data;
+	
+	//default constructor
+	//which in turn calls the parameterized constructor
+	public Stack() {
+		this(5); //used to call the other constructor within another constructor
+	}
+	
+	
+	// Parameterized constructor
+	// would create more confusion for the user
+	public Stack(int cap) {
+		this.tos = -1;
+		this.data = new int[cap];
+	}
 }

48a87e46caab15f1873c5bfe2717b69ba558185c
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-05-05 15:58:08 +0530

    push operation

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -7,14 +7,19 @@ public class Stack {
 	//default constructor
 	//which in turn calls the parameterized constructor
 	public Stack() {
-		this(5); //used to call the other constructor within another constructor
+		this(5); //used to call the other constructor within another constructor 
 	}
-	
 	
 	// Parameterized constructor
 	// would create more confusion for the user
 	public Stack(int cap) {
 		this.tos = -1;
 		this.data = new int[cap];
+	}
+	
+	public void push(int item) {
+		this.tos ++ ;
+		this.data[this.tos] = item;
+		
 	}
 }

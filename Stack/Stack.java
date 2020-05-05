2c23e7919263307be8495214897514ef9256701f
Manan Arora <31929934+manan05@users.noreply.github.com>
2020-05-05 16:14:46 +0530

    pop()

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -3,23 +3,31 @@ package Stack;
 public class Stack {
 	int tos;
 	int[] data;
-	
-	//default constructor
-	//which in turn calls the parameterized constructor
+
+	// default constructor
+	// which in turn calls the parameterized constructor
 	public Stack() {
-		this(5); //used to call the other constructor within another constructor 
+		this(5); // used to call the other constructor within another constructor
 	}
-	
+
 	// Parameterized constructor
 	// would create more confusion for the user
 	public Stack(int cap) {
 		this.tos = -1;
 		this.data = new int[cap];
 	}
-	
+
+	// this operation pushes the item into the stack
 	public void push(int item) {
-		this.tos ++ ;
+		this.tos++; // increments tos first b/c initially it is -1
 		this.data[this.tos] = item;
-		
+	}
+
+	// this operation pops the item on tos
+	public int pop() {
+		int re = this.data[this.tos];
+		this.data[this.tos] = 0;
+		this.tos--;
+		return re;
 	}
 }

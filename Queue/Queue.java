82fba9f3fc9b96fe3aaadbe1a028ddff59b2fd4e
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-07 03:24:21 +0530

    added Dequeue(), getFirst(), size(), isEmpty()

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -17,13 +17,36 @@ public class Queue {
 		this(5); // calling the parameterized constructor for value 5
 	}
 
-	//This method adds elements to the queue
+	// This method adds elements to the queue
 	public void enqueue(int item) {
-		if()
-		
-		int ni = (this.front + this.size) % this.data.length; //to make the queue circular
-		this.data[ni] = item; 
+		// ni = new index
+		int ni = (this.front + this.size) % this.data.length; // to make the queue circular
+		this.data[ni] = item;
 		this.size++;
 	}
 
+	// This method removes element from the queue and displays that elem
+	public int dequeue() {
+		int rv = this.data[this.front]; // rv = return value
+		this.data[this.front] = 0; // set the front value as 0
+		this.front = (this.front + 1) % this.data.length; // to make dequeue circular
+		this.size--;
+		return rv;
+	}
+
+	// This method returns the first element of the queue
+	public int getFirst() {
+		int rv = this.data[this.front]; // rv = return value
+		return rv;
+	}
+
+	// This method returns the size of the queue
+	public int size() {
+		return this.size;
+	}
+
+	// This method returns whether the queue is empty or not
+	public boolean isEmpty() {
+		return this.size() == 0;
+	}
 }

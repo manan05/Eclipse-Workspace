b38b86df3bcb7260ad2a6aa2b74165659717f8e7
manan05 <mananarora313@gmail.com>
2020-07-02 23:17:18 +0530

    added files

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -5,10 +5,40 @@
  */
 package LinkedList;
 
-/**
- * @author Manan
- *
- */
 public class LinkedList {
+
+	// Creating a private Node class so that client cannot
+	// access it.
+	private class Node {
+		int data; // data of the node
+		Node next; // address of the next node
+	}
+
+	// Properties of the LL
+	private Node head; // points at the first node of the LL
+	private Node tail; // points at the last node of the LL
+	private int size;
+
+	
+	public int size() {
+		return this.size;
+	}
+	
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+	
+	public void addLast(int item) {
+		
+		// Create a new node
+		Node newNode = new Node();
+		
+		// Update 
+		newNode.data = item;
+		newNode.next = null;
+		
+		// Attach
+		
+	}
 
 }
---
+++ LinkedList/LinkedListClient.java
@@ -0,0 +1,15 @@
+/**
+ * @author Manan Arora
+ * @email mananarora313@gmail.com
+ * @date 02-Jul-2020
+ */
+package LinkedList;
+
+public class LinkedListClient {
+	
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		list.addLast(10);
+	}
+}

26c1b882731d0acaf5e3c7d314fbf966569d5bf2
manan05 <mananarora313@gmail.com>
2020-08-23 18:19:58 +0530

    .

--- Array/SubArrayWithGivenSum.java
+++ Array/SubArrayWithGivenSum.java
@@ -16,10 +16,33 @@ package Array;
 //4. if the sum is equal to the target print the sub array.
 public class SubArrayWithGivenSum {
 
+	public boolean subArraySum(int[] arr , int n, int sum) {
+		int currentSum = 0;
+		for(int i = 0; i<n;i++) {
+			currentSum = arr[i];
+			for(int j = i+1 ; j<n;j++) {
+				if(currentSum == sum) {
+					System.out.println("Found between " + i + " and " + (j-1));
+					return true;
+				}
+				if(currentSum > sum || j == n) {
+					break;
+				}
+				currentSum = currentSum + arr[j];
+			}
+		}
+		System.out.println("Not found.");
+		return false;
+	}
+	
 	
 	public static void main(String[] args) {
 		
-		
+		SubArrayWithGivenSum ab = new SubArrayWithGivenSum();
+		int[] arr = {15, 2, 4, 8, 9, 5, 10, 23};
+		int n = arr.length;
+		int sum = 24;
+		System.out.println(ab.subArraySum(arr, n, sum));
 	}
 
 }

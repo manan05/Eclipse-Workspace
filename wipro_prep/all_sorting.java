75812147474b5df48849959ffc5e29cc1bd1aeba
manan05 <mananarora313@gmail.com>
2021-01-22 22:42:25 +0530

    Updated

--- wipro_prep/all_sorting.java
+++ wipro_prep/all_sorting.java
@@ -14,7 +14,8 @@ public class all_sorting {
 		}
 		printArray(arr);
 //		selectionSort(arr);
-		bubbleSort(arr);
+//		bubbleSort(arr);
+		insertionSort(arr);
 		System.out.println();
 		printArray(arr);
 
@@ -52,8 +53,16 @@ public class all_sorting {
 		}
 	}
 	
-	public static void insertionSort() {
-		
+	public static void insertionSort(int[] arr) {
+		for(int i = 1; i<arr.length; i++) {
+			int key = arr[i];
+			int j = i-1;
+			while(j>=0 && arr[j]>key) {
+				arr[j+1] = arr[j];
+				j=j-1;
+			}
+			arr[j+1] = key;
+		}
 	}
 
 }

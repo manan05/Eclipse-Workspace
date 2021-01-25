6750e52e9773f10a8437a0a01514af68be264664
manan05 <mananarora313@gmail.com>
2021-01-26 01:54:38 +0530

    .

--- wipro_prep/GreatestCommonDivisor.java
+++ wipro_prep/GreatestCommonDivisor.java
@@ -1,10 +1,25 @@
 package wipro_prep;
 
+import java.util.Scanner;
+
 public class GreatestCommonDivisor {
-
+	
 	public static void main(String[] args) {
-		
-
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		System.out.println(gcd(a,b));
+	}
+	public static int gcd(int a, int b) {
+		while(a!=b) {
+			if(a>b) {
+				a = a-b;
+			}
+			else {
+				b = b-a;
+			}
+		}
+		return a;
 	}
 
 }

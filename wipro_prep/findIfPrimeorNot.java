d7855f3c1f2d43af9210771c914bc619f2a2fc14
manan05 <mananarora313@gmail.com>
2021-01-27 18:05:50 +0530

    updated findPrimeorNot

--- wipro_prep/findIfPrimeorNot.java
+++ wipro_prep/findIfPrimeorNot.java
@@ -1,10 +1,32 @@
 package wipro_prep;
 
+import java.util.Scanner;
+
 public class findIfPrimeorNot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println(isPrime(n));
+	}
 
+	public static boolean isPrime(int n) {
+		if (n == 0 || n == 1) {
+			return false;
+		} else {
+			int flag = 0;
+			for (int i = 2; i * i <= n; i++) {
+				if(n%i == 0) {
+					flag = 1;
+				}
+			}
+			if(flag == 1) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
 	}
 
 }

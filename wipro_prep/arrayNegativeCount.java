9ce6edcebf5c487533c7ae0755d0e8f34d30a5dc
manan05 <mananarora313@gmail.com>
2021-01-30 22:49:28 +0530

    added arrayNegativeCount

---
+++ wipro_prep/arrayNegativeCount.java
@@ -0,0 +1,26 @@
+package wipro_prep;
+
+import java.util.Scanner;
+
+public class arrayNegativeCount {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i =0; i <n;i++) {
+			arr[i] = scn.nextInt();
+		}
+		System.out.println(arrayNegativeCountPrint(arr));
+	}
+	public static int arrayNegativeCountPrint(int[] arr) {
+		int count = 0;
+		for(int i =0; i<arr.length; i++) {
+			if(arr[i] < 0) {
+				count ++;
+			}
+		}
+		return count;
+	}
+
+}

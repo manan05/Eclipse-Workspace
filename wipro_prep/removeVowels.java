19502fcda8ec5615ed04b4fc32cf1f69d5f8f697
manan05 <mananarora313@gmail.com>
2021-01-30 21:04:31 +0530

    updated removeVowels

--- wipro_prep/removeVowels.java
+++ wipro_prep/removeVowels.java
@@ -7,13 +7,16 @@ public class removeVowels {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		String str = scn.nextLine();
-		char[] arr = str.toCharArray();
-		remove(arr);
+//		char[] arr = str.toCharArray();
+		remove(str);
 	}
-	public static void remove(char[] arr) {
-		for(int i =0; i<arr.length; i++) {
-			if(arr[i] != 'a' || arr[i] != 'e'|| arr[i] != 'i' || arr[i] != 'o'|| arr[i] != 'u') {
-				System.out.print(arr[i]);
+	public static void remove(String str) {
+		for(int i =0; i<str.length(); i++) {
+			if(str.charAt(i) == 'a' || str.charAt(i)== 'e' || str.charAt(i)== 'i'|| str.charAt(i)== 'o'|| str.charAt(i)== 'u') {
+				continue;
+			}
+			else {
+				System.out.print(str.charAt(i));
 			}
 		}
 	}

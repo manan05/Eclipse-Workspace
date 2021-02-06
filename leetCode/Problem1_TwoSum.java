f24c2290b24b36975db911cf4c0d449cb854b8c5
manan05 <mananarora313@gmail.com>
2021-02-06 17:03:57 +0530

    added Problem#1

--- leetCode/Problem1_TwoSum.java
+++ leetCode/Problem1_TwoSum.java
@@ -1,10 +1,43 @@
 package leetCode;
 
+import java.util.Scanner;
+
+// #1 Two Sum
+// Link = https://leetcode.com/problems/two-sum/
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 public class Problem1_TwoSum {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] nums = new int[n];
+		for(int i = 0; i<n;i++) {
+			nums[i] = scn.nextInt();
+		}
+		int target = scn.nextInt();
+		int[] ret = twoSum(nums, target);
+		printArray(ret);
+		
+	}
+	public static int[] twoSum(int[] nums, int target) {
+		int[] ans = new int[2];
+		for(int i = 0, j = 1; i<nums.length && j<nums.length; i++,j++) {
+			if(nums[i]+nums[j] == target) {
+				ans[0] = i;
+				ans[1] = j;
+			}
+		}
+		return ans;
+	}
+	public static void printArray(int[] arr) {
+		for(int i = 0; i<arr.length; i++) {
+			System.out.print( arr[i] + " ");
+		}
 	}
 
 }

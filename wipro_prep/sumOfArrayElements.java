33de7bcf70182756d6ed9ab6cb8eb07e3e43d479
Manan Arora <mananarora313@gmail.com>
2021-03-04 23:42:15 +0530

    update

--- wipro_prep/sumOfArrayElements.java
+++ wipro_prep/sumOfArrayElements.java
@@ -3,29 +3,27 @@ package wipro_prep;
 import java.util.Scanner;
 
 public class sumOfArrayElements {
-
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		int[] arr = new int[n];
-		for(int i =0; i<n;i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = scn.nextInt();
 		}
-		sumArr(arr);
+		sumOfArrayElems(arr);
 	}
-	public static void sumArr(int[] arr) {
-		
-		for(int i = 0; i<arr.length;i++) {
+
+	public static void sumOfArrayElems(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
 			int sum = 0;
 			int num = arr[i];
-			while(num!=0) {
-				int rem = num%10;
+			int temp = num;
+			while (temp != 0) {
+				int rem = temp % 10;
 				sum = sum + rem;
-				num = num/10;
+				temp = temp / 10;
 			}
 			System.out.print(sum + " ");
 		}
-		
 	}
-
 }

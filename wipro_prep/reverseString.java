7ad3194d8bde3a02282820e59360e67402bee4f2
manan05 <mananarora313@gmail.com>
2021-03-12 17:41:46 +0530

    updated reverseString

--- wipro_prep/reverseString.java
+++ wipro_prep/reverseString.java
@@ -1,30 +1,25 @@
 package wipro_prep;
-
+import java.util.*;
 import java.util.Scanner;
 
-public class reverseString {
-
+public class reverseString{
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		String str = scn.nextLine();
-		reverse(str);
+		System.out.println(reverse(str));
 	}
-
-	public static void reverse(String str) {
+	public static String reverse(String str) {
 		char[] arr = str.toCharArray();
 		int i = 0;
-		int j = arr.length - 1;
-		str.toUpperCase();
-		System.out.println(str);
-		while (i <= j) {
+		int j = arr.length-1;
+		while(i<=j) {
 			char temp = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp;
 			i++;
 			j--;
 		}
-		for (char ch : arr) {
-			System.out.print(ch);
-		}
+		String ans = String.valueOf(arr);
+		return ans;
 	}
 }
